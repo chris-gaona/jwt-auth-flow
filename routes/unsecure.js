e73896afb9c2fa2189e1714e03c4b130e1c0b1fa
Chris Gaona <thebeloved88@sbcglobal.net>
2022-07-12 14:56:35 -0700

    Adds refresh token route

--- routes/unsecure.js
+++ routes/unsecure.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.post('/ping', (req, res, next) => {
   res.json({
     message: 'pong',
-  });
+  })
 })
 
 router.post(
@@ -17,7 +17,7 @@ router.post(
     res.json({
       message: 'Signup successful',
       user: req.user
-    });
+    })
   }
 )
 
@@ -29,28 +29,63 @@ router.post(
       async (err, user, info) => {
         try {
           if (err || !user) {
-            const error = new Error('An error occurred.');
-
-            return next(error);
+            const error = new Error('An error occurred.')
+            return next(error)
           }
 
           req.login(
             user,
             { session: false },
             async (error) => {
-              if (error) return next(error);
+              if (error) return next(error)
 
-              const body = { _id: user._id, email: user.email };
-              const token = jwt.sign({ user: body }, process.env.SECRET_KEY);
+              const body = { _id: user._id, email: user.email }
+              const token = jwt.sign({ user: body }, process.env.SECRET_KEY, {
+                expiresIn: '1m'
+              })
+              const refreshToken = jwt.sign({ user: body }, process.env.REFRESH_KEY, {
+                expiresIn: '3m'
+              })
 
-              return res.json({ token });
+              // Send the refreshToken in an httpOnly cookie
+              res.cookie('refreshToken', refreshToken, { httpOnly: true })
+
+              return res.json({ token })
             }
-          );
+          )
         } catch (error) {
-          return next(error);
+          return next(error)
         }
       }
-    )(req, res, next);
+    )(req, res, next)
+  }
+)
+
+router.get('/refresh-token',
+  async (req, res, next) => {
+    const refreshToken = req.cookies.refreshToken
+    if (!refreshToken) {
+      const error = new Error(`refeshToken cookie not found.`)
+      return next(error)
+    }
+    try {
+      // Handle refreshing the access token if the refresh token is still valid
+      const parsedRefreshToken = jwt.verify(refreshToken, process.env.REFRESH_KEY)
+      
+      if (!parsedRefreshToken) {
+        const error = new Error(`The token could not be parsed.`)
+        return next(error)
+      }
+  
+      const token = jwt.sign({ user: parsedRefreshToken.user }, process.env.SECRET_KEY, {
+        expiresIn: '1m'
+      })
+  
+      return res.json({ token })
+    } catch (error) {
+      return next(error)
+    }
+
   }
 )
 
