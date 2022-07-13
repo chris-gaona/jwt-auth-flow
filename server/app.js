c9f022479417e64660e44375d90b8e9c6afdcb94
Chris Gaona <thebeloved88@sbcglobal.net>
2022-07-13 13:47:16 -0700

    Adds some more useful links for reference

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@
   https://github.com/auth0/node-jsonwebtoken
 
   https://jakeowen-ex.medium.com/secure-api-authentication-with-nextjs-http-only-cookie-b4ea0569bbfc
+
+  https://medium.com/@ryanchenkie_40935/react-authentication-how-to-store-jwt-in-a-cookie-346519310e81
+
+  https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#basics-login
 */
 
 const express = require('express')
