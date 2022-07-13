cf92ad9b7844804077b54986d29f7dc420fc79fd
Chris Gaona <thebeloved88@sbcglobal.net>
2022-07-13 13:48:50 -0700

    Adds reference links to the README instead

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # jwt-auth-flow
+
+Links used as a reference in creating this authentication flow:
+
+https://www.digitalocean.com/community/tutorials/api-authentication-with-json-web-tokensjwt-and-passport
+  
+https://github.com/auth0/node-jsonwebtoken
+
+https://jakeowen-ex.medium.com/secure-api-authentication-with-nextjs-http-only-cookie-b4ea0569bbfc
+
+https://medium.com/@ryanchenkie_40935/react-authentication-how-to-store-jwt-in-a-cookie-346519310e81
+
+https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#basics-login
--- server/app.js
+++ server/app.js
@@ -1,15 +1,3 @@
-/*
-  https://www.digitalocean.com/community/tutorials/api-authentication-with-json-web-tokensjwt-and-passport
-  
-  https://github.com/auth0/node-jsonwebtoken
-
-  https://jakeowen-ex.medium.com/secure-api-authentication-with-nextjs-http-only-cookie-b4ea0569bbfc
-
-  https://medium.com/@ryanchenkie_40935/react-authentication-how-to-store-jwt-in-a-cookie-346519310e81
-
-  https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#basics-login
-*/
-
 const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
